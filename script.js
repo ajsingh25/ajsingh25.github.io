5ba67fb363e273a1dd141ac010121fd558684417
AJ Singh <ams992@scarletmail.rutgers.edu>
2024-10-23 15:02:29 -0400

    submission1

---
+++ script.js
@@ -0,0 +1,9 @@
+function setDarkMode() {
+    document.body.style.backgroundColor = "#333";
+    document.body.style.color = "white";
+}
+
+function setLightMode() {
+    document.body.style.backgroundColor = "#f0f0f0";
+    document.body.style.color = "#333";
+}
